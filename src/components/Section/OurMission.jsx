10abe1e39d4ad45aa873d1ca4e9efda10d64bc4d
Prabesh <prabesh5511@gmail.com>
2025-08-12 13:33:51 +0545

    update ourMission.jsx

--- src/components/Section/OurMission.jsx
+++ src/components/Section/OurMission.jsx
@@ -5,12 +5,31 @@ const MissionSection = () => {
     <section className="w-full  px-4 ">
       <div className=" mx-auto text-center">
         {/* Heading */}
-        <h2 className="text-3xl pt-40 md:text-4xl font-bold text-[#24A69A] tracking-wide mb-4">
+        {/* <h2 className="text-3xl pt-40 md:text-4xl font-bold text-[#24A69A] tracking-wide mb-4">
           OUR MISSION
         </h2>
 
-        {/* Divider */}
-        <div className="w-50 h-1  bg-[#e98524] mx-auto mb-6"></div>
+        <div className="w-50 h-1  bg-[#e98524] mx-auto mb-6"></div> */}
+
+
+<div
+            style={{ color: "var(--color-title)" }}
+            className="text-3xl pt-40 md:text-4xl font-bold text-[#24A69A] tracking-wide mb-10"
+          >
+            <div className="inline-block">
+              OUR MISSION 
+              <div
+                // style={{ backgroundColor: "var(--color-title2)" }}
+                className="h-[4px] mt-1 bg-[#e98524] w-full "
+              ></div>
+            </div>
+          </div>
+
+
+
+
+
+
 
         {/* Paragraph */}
         <p className="text-lg md:text-2xl max-w-xl mx-auto text-black mb-10 leading-relaxed">
